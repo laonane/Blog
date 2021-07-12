89d988d7e0ac170c07b76b906e2cfa7479eb442e
HuaiAnGG <971375353@qq.com>
2021-07-12 22:37:17 +0800

    搭建框架

--- blog-boot/src/test/java/wiki/laona/boot/BlogBootApplicationTests.java
+++ blog-boot/src/test/java/wiki/laona/boot/BlogBootApplicationTests.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import wiki.laona.boot.mapper.ArticleMapper;
-import wiki.laona.boot.pojo.Article;
+import wiki.laona.boot.mapper.CategoryMapper;
+import wiki.laona.boot.mapper.CustomerMapper;
+import wiki.laona.boot.pojo.dao.Article;
+import wiki.laona.boot.pojo.dao.Category;
+import wiki.laona.boot.pojo.dao.Customer;
 
 import java.util.List;
 
@@ -13,11 +17,30 @@ class BlogBootApplicationTests {
 
     @Autowired
     private ArticleMapper articleMapper;
+    @Autowired
+    private CategoryMapper categoryMapper;
+    @Autowired
+    private CustomerMapper customerMapper;
 
     @Test
     void contextLoads() {
         List<Article> articles = articleMapper.selectList(null);
         articles.forEach(System.out::println);
+
+        List<Category> categories = categoryMapper.selectList(null);
+        categories.forEach(System.out::println);
+
+        List<Customer> customers = customerMapper.selectList(null);
+        customers.forEach(System.out::println);
+    }
+
+
+    @Test
+    void addCustomerTest() {
+        Customer customer = new Customer();
+        customer.setUsername("laona");
+        customer.setPassword("123");
+        customerMapper.insert(customer);
     }
 
 }
